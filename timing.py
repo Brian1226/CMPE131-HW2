e75ee23c29d380dcc98818a7220462d672bc98bd
Brian1226 <79072222+Brian1226@users.noreply.github.com>
2021-03-05 23:35:17 -0800

    updated timing.py

--- timing.py
+++ timing.py
@@ -1,21 +1,23 @@
+
 import time
 """"
 Uses decorator to determine current time, pause program for 2 seconds, then calculate the difference between the time before the sleep & and time after the sleep.
 
 Parameters:
-current : the time before the sleep (float)
-time.time() : the time used after the sleep (float)
+initial_time : the time before the sleep (float)
+final_time : the time used after the sleep (float)
 
 Example:
-current = 1615015080.3982704
-time.time() = 1615015082.400343
+inital_time = 1615015080.3982704
+final_time = 1615015082.400343
 total_time = 2.0021908283233643
 """
 def calculate_time(func):
   def wrapper():
-    current = time.time()
+    inital_time = time.time()
     func()
-    total_time = time.time() - current
+    final_time = time.time()
+    total_time = final_time - inital_time
     print(f'Total time {total_time}')
   return wrapper
 
