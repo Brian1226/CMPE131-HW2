f1eb49641e5720a2961dd5db0487a78aac6db9e4
Brian1226 <79072222+Brian1226@users.noreply.github.com>
2021-03-05 16:53:50 -0800

    added investment.py to repository

---
+++ investment.py
@@ -0,0 +1,41 @@
+def calculate_apr():
+  """
+  This function calculates total money earned from investment
+  using the formula principal * (1 + interest_rate)
+
+  Parameters:
+  principal : int
+  interest_rate : float
+  years : int
+
+  Returns:
+  total
+    total =  principal * (1 + interest_rate)
+
+  Example:
+  principal = 500
+  interest_rate = 0.05
+  years = 10
+  The total should return 5250.0
+
+  But if any of the parameters is negative, returns False
+  """
+  #make sure that principal and years is input is int, and interest_rate input is float
+  principal = int(input()) 
+  interest_rate = float(input())
+  years = int(input())
+  total = 0
+  for i in range(years):
+    total = total + (principal * (1 + interest_rate))
+
+  #if any of the parameters is negative return False
+  if principal < 0:
+    return False
+  elif interest_rate < 0:
+    return False
+  elif years < 0:
+    return False
+  else:
+    print(total)
+  
+calculate_apr()
