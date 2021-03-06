40017ecdf19d91b5ff508ad9521b30184e356fee
Brian1226 <79072222+Brian1226@users.noreply.github.com>
2021-03-05 20:57:26 -0800

    updated investment.py

--- investment.py
+++ investment.py
@@ -25,7 +25,7 @@ def calculate_apr(principal, interest_rate, years):
   
   if principal < 0:
     return False
-  if interest_rate < 0:
+  if interest_rate <= 0
     return False
   if years < 0:
     return False
