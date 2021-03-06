95049c934650f620203a724a214b6e26bb8a3569
Brian1226 <79072222+Brian1226@users.noreply.github.com>
2021-03-05 20:41:21 -0800

    updated investment.py

--- investment.py
+++ investment.py
@@ -20,7 +20,7 @@ def calculate_apr(principal, interest_rate, years):
 
   But if any of the parameters is negative, returns False
   """
-  #make sure that principal and years is input is int, and interest_rate input is float
+
   total = 0
   
   if principal < 0:
@@ -30,7 +30,7 @@ def calculate_apr(principal, interest_rate, years):
   if years < 0:
     return False
   
-  if isinstance(interest_rate, int) or isinstance(interest_rate, float) and isinstance(principal, int) or isinstance(principal, float):
+  if isinstance(years, int) and isinstance(interest_rate, float) and isinstance(principal, int) or isinstance(principal, float):
     for i in range(years):
       total = total + (principal * (1 + interest_rate))
   else:
