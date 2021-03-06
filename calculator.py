b89484584e86ae55e4ffaa93697b93f3aed5cdac
Brian1226 <79072222+Brian1226@users.noreply.github.com>
2021-03-05 21:25:19 -0800

    updated calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 def calculator(number1, number2, operator):
+  
   if operator == "+":
     output = (number1 + number2)
   elif operator == "-":
@@ -18,10 +19,10 @@ def calculator(number1, number2, operator):
 
 def input_output():
  
-  number1 = input("Enter the first number: ")
-  number1 = float(number1)
-  number2 = input("Enter the second number: ")
-  number2 = float(number2)
+  number1 = float(input("Enter the first number: "))
+  
+  number2 = float(input("Enter the second number: "))
+  
   operator = input("Enter the operation: ")
   print(calculator(number1, number2, operator))
   next = input("Do you wish to exit: ")
@@ -39,4 +40,3 @@ def input_output():
       break
     
 input_output()
-  
