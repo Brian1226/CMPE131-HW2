e1ab461e43880b6b0bdc98dbb89eb210c1745de9
Brian1226 <79072222+Brian1226@users.noreply.github.com>
2021-03-05 22:48:07 -0800

    updated timing.py

--- timing.py
+++ timing.py
@@ -2,13 +2,11 @@ import time
 
 def calculate_time(func):
   def wrapper():
-    print(time.time())
     func()
   return wrapper
 
 @calculate_time
 def time_sleep():
-  return(time.sleep(2))
-
+  print(time.time())
 
 
